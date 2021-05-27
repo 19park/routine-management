8abe38dc9622a360a3b6df570e13acd907df84b3
박정환 <19park@wholedoc.net>
2021-05-27 19:36:09 +0900

    setting splash/login/register

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler'
 import React from 'react'
-import MainNavigation from './navigation/navigation'
+import Navigation from './navigation/navigation'
 import {Provider} from 'react-redux'
 import {store} from './stores'
 import {enableScreens} from 'react-native-screens'
@@ -11,7 +11,7 @@ const App = () => {
     return (
         <>
             <Provider store={store}>
-                <MainNavigation/>
+                <Navigation/>
             </Provider>
         </>
     )
--- client/src/navigation/navigation.js
+++ client/src/navigation/navigation.js
@@ -1,50 +1,73 @@
-import React from 'react'
-import Login from '../screens/Login/Login.screen'
-import Home from '../screens/Home/Home.screen'
-import Profile from '../screens/Profile/Profile.screen'
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-import Icon from 'react-native-vector-icons/Ionicons'
+import 'react-native-gesture-handler';
+
+// Import React and Component
+import React from 'react';
+
+// Import Navigators from React Navigation
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+// Import Screens
+import SplashScreen from '../screens/Splash/Index';
+import LoginScreen from '../screens/Login/Index';
+import RegisterScreen from '../screens/Register/Index';
+import DrawerNavigationRoutes from '../navigation/drawerRoute';
 
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
 
-function MyTabs() {
+const Auth = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen
-                name="Home"
-                component={Home}
+        <Stack.Navigator initialRouteName="LoginScreen">
+            <Stack.Screen
+                name="LoginScreen"
+                component={LoginScreen}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name="RegisterScreen"
+                component={RegisterScreen}
                 options={{
-                    tabBarIcon: ({focused, color, size}) => {
-                        return <Icon name={'ios-home'} size={25} color={color}/>
-                    }
+                    title: 'Register', //Set Header Title
+                    headerStyle: {
+                        backgroundColor: '#307ecc', //Set Header color
+                    },
+                    headerTintColor: '#fff', //Set Header text color
+                    headerTitleStyle: {
+                        fontWeight: 'bold', //Set Header text style
+                    },
                 }}
             />
-            <Tab.Screen
-                name="Profile"
-                component={Profile}
-                options={{
-                    tabBarIcon: ({focused, color, size}) => {
-                        return <Icon name={'ios-settings'} size={25} color={color}/>
-                    }
-                }}
-            />
-        </Tab.Navigator>
-    )
-}
-
-const MainNavigation = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator headerMode="none">
-                <Stack.Screen name="Login" component={Login}/>
-                {/*<Stack.Screen name="Home" component={MyTabs}/>*/}
-                {/* add your another screen here using -> Stack.Screen */}
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+        </Stack.Navigator>
+    );
 };
 
-export default MainNavigation
+const Navigation = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="SplashScreen">
+                {/* SplashScreen which will come once for 5 Seconds */}
+                <Stack.Screen
+                    name="SplashScreen"
+                    component={SplashScreen}
+                    // Hiding header for Splash Screen
+                    options={{headerShown: false}}
+                />
+                {/* Auth Navigator: Include Login and Signup */}
+                <Stack.Screen
+                    name="Auth"
+                    component={Auth}
+                    options={{headerShown: false}}
+                />
+                {/* Navigation Drawer as a landing page */}
+                <Stack.Screen
+                    name="DrawerNavigationRoutes"
+                    component={DrawerNavigationRoutes}
+                    // Hiding header for Navigation Drawer
+                    options={{headerShown: false}}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default Navigation;
